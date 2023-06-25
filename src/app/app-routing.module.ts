71b4a08a0ba167def5d4e22fc60ace218b8dd116
AgnesGehle <agnes.gehle@bitskaten.de>
2023-06-25 21:56:51 +0200

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {EmployeeListComponent} from "./employees/employee-list/employee-list.component";
+import {AddEmployeeComponent} from "./employees/add-employee/add-employee.component";
+import {UpdateEmployeeComponent} from "./employees/update-employee/update-employee.component";
+import { Routes, RouterModule } from "@angular/router";
 
+const routes: Routes = [
+  {path: '', component: EmployeeListComponent},
+  {path: 'add-employee', component: AddEmployeeComponent},
+  {path: 'update-employee', component: UpdateEmployeeComponent}
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
